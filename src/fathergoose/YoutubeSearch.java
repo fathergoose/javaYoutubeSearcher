2497d56843c6101852163f05ea32133ff9868b0d
fathergoose <al@ilseman.com>
2016-03-20 13:02:41 -0500

    printing firs cli arg

--- src/fathergoose/YoutubeSearch.java
+++ src/fathergoose/YoutubeSearch.java
@@ -5,6 +5,8 @@ public class YoutubeSearch {
 	static final String apiKey = System.getenv("YOUTUBE_API_KEY");
 
 	public static void main(String[] args) {
+		String query = args[0];
+		System.out.println(query);
 		System.out.println(apiKey);
 
 	}
