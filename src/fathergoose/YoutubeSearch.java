d191b199930d45323b6b4b3c83914aa3b1a64840
fathergoose <al@ilseman.com>
2016-03-20 15:59:39 -0500

    removed extra printing and put args[0] back in

--- src/fathergoose/YoutubeSearch.java
+++ src/fathergoose/YoutubeSearch.java
@@ -36,12 +36,10 @@ public class YoutubeSearch {
 	}
 
 	public static void main(String[] args) throws IOException {
-		YoutubeSearch search = new YoutubeSearch("hotdogs");
+		YoutubeSearch search = new YoutubeSearch(args[0]);
 		for (Video vid : search.videos) {
 			System.out.println(vid.getUrl());
 		}
-		System.out.println(search.query);
-		System.out.println(apiKey);
 
 	}
 
